9aa44beeadd44175d33c3309fd241c06574e0530
d.b.robinson@lancaster.ac.uk <d.b.robinson@lancaster.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-07-06 16:26:05 +0000

    properties converted to JavaScript
    git-svn-id: https://source.sakaiproject.org/svn/roster/branches/trimpath_work@79105 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ roster-app/src/webapp/roster-translations-en-gb.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2008-2010 The Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// UK changes
+profile_site_roster="List of Members";
+title_show_roster="List of Members";
+print_title_for="List of Members for";
+print_title="List of Members";
+title_msg=To add or remove participants from the site, visit the Site Info tool.";
+
+profile_school="Division";
+roster_title="Site Members";
+no_permission_msg="You do not have permission to view the list of site members.";
+facet_roster_list="List of Site Members";
+not_found_roster_profile="Member Profile";
+export_filename_prefix="site_members";
