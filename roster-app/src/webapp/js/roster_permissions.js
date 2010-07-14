25f7ff76d692c20b747d6286495a0364037b4854
d.b.robinson@lancaster.ac.uk <d.b.robinson@lancaster.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-07-14 16:08:02 +0000

    added remaining permissions
    git-svn-id: https://source.sakaiproject.org/svn/roster/branches/trimpath_work@79438 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- roster-app/src/webapp/js/roster_permissions.js
+++ roster-app/src/webapp/js/roster_permissions.js
@@ -1,3 +1,5 @@
+// See http://confluence.sakaiproject.org/display/SRT/Default+permissions
+
 function RosterPermissions(data) {
 	
 	for(var i = 0, j = data.length; i < j; i++) {
@@ -6,13 +8,18 @@ function RosterPermissions(data) {
 			this.rosterExport = true;
 		} else if ('roster.viewallmembers' === data[i]) {
 			this.viewAllMembers = true;
+		} else if ('roster.viewenrollmentstatus' === data[i]) {
+			this.viewEnrollmentStatus = true;
 		} else if ('roster.viewgroup' === data[i]) {
 			this.viewGroup = true;
+		} else if ('roster.viewhidden' === data[i]) {
+			this.viewHidden = true;
+		} else if ('roster.viewofficialphoto' === data[i]) {
+			// TODO this may be removed soon
+			this.viewOfficialPhoto = true;
 		} else if ('roster.viewprofile' === data[i]) {
 			this.viewProfile = true;
 		}
-		
-		// TODO check if any missing.
 	}
 		
 }
